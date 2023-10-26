5658f84bd7f519c06ae55cf25bed702df9642559
modelinglaboratory <78147681+modelinglaboratory@users.noreply.github.com>
2023-10-26 20:35:04 +0300

    Update user_add_wireguard.sh

--- user_add_wireguard.sh
+++ user_add_wireguard.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
+# Check if wg command exists (WireGuard tools)
+if ! command -v wg &> /dev/null; then
+    echo "WireGuard is not installed. Installing now..."
+    
+    # Assuming you are using a Debian-based system
+    sudo apt update
+    sudo apt install -y wireguard wireguard-tools
+fi
+
 # Path to WireGuard configuration file
 WG_CONFIG="/etc/wireguard/wg0.conf"
+
+# If the WireGuard configuration file doesn't exist, create it
+if [ ! -f "$WG_CONFIG" ]; then
+    echo "[Interface]
+Address = 10.0.0.1/24
+ListenPort = 51820
+PrivateKey = $(wg genkey)" > $WG_CONFIG
+
+    # Enable and start the WireGuard service
+    sudo systemctl enable wg-quick@wg0
+    sudo systemctl start wg-quick@wg0
+fi
 
 # Get the server's public key and endpoint information from the user
 read -p "Server's Public Key: " SERVER_PUBLIC_KEY
@@ -12,26 +33,11 @@ PRIVATE_KEY=$(wg genkey)
 PUBLIC_KEY=$(echo "$PRIVATE_KEY" | wg pubkey)
 
 # Determine the user's IP address (in this example, it's in the format 10.0.0.X)
-# Optionally get the IP address from the user
 read -p "User IP Address (leave empty for automatic assignment, e.g., 10.0.0.5): " CLIENT_IP
-
-# If the user IP address is not specified, assign it automatically
 if [ -z "$CLIENT_IP" ]; then
     LAST_IP=$(grep AllowedIPs $WG_CONFIG | awk -F'.' '{print $4}' | awk -F'/' '{print $1}' | sort -n | tail -1)
     CLIENT_IP="10.0.0.$((LAST_IP + 1))"
 fi
-
-# Create the WireGuard configuration for the new user
-echo "[Interface]
-PrivateKey = $PRIVATE_KEY
-Address = $CLIENT_IP/32
-DNS = 10.0.0.1
-
-[Peer]
-PublicKey = $SERVER_PUBLIC_KEY
-Endpoint = $SERVER_ENDPOINT
-AllowedIPs = 0.0.0.0/0
-PersistentKeepalive = 25" > "$CLIENT_IP-wg0.conf"
 
 # Add the new user to the server configuration file
 echo -e "\n[Peer]
@@ -41,7 +47,7 @@ AllowedIPs = $CLIENT_IP/32" >> $WG_CONFIG
 # Restart the server
 wg syncconf wg0 <(wg-quick strip wg0)
 
-echo "New user successfully added. Configuration file: $CLIENT_IP-wg0.conf"
+echo "New user successfully added to the server. Now, displaying client configuration."
 
 # Display client configuration details
 echo -e "\n[Interface]
